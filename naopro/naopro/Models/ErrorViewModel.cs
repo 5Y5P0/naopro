0349a24f23e4442361e0ac2f3c3bda3bce5192ee
FOUCHER <sebastien.foucher@live.com>
2019-08-05 12:00:12 +0200

    nouveau projet

---
+++ naopro/naopro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace naopro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
