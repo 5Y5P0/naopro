7c651cac455be86677461e3c4c38476cee5223da
FOUCHER <sebastien.foucher@live.com>
2020-01-10 18:53:26 +0100

    tentative d'intégrer web component

---
+++ naopro/WebApp/Components/PagerComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Components
+{
+    public class PagerComponent : ComponentBase
+    {
+        [Parameter] public int PageNumber { get; set; }
+        [Parameter] public int TotalRecords { get; set; }
+        [Parameter] public int PageSize { get; set; } = 20;
+        [Parameter] public string LinkUrl { get; set; }
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalRecords / PageSize);
+    }
+
+}
--- naopro/WebApp/Startup.cs
+++ naopro/WebApp/Startup.cs
@@ -56,6 +56,8 @@ namespace WebApp
 
             services.AddTransient<SharedViewModel>();
 
+            services.AddServerSideBlazor();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
