f3f5732294d202d9ef402a25656cdac9bc78c051
FOUCHER <sebastien.foucher@live.com>
2020-01-25 14:30:31 +0100

    ajout sit guibout ravalement

--- naopro/WebApp/Models/AppVersionService.cs
+++ naopro/WebApp/Models/AppVersionService.cs
@@ -9,17 +9,33 @@ namespace WebApp.Models
     public class AppVersionService : IAppVersionService
     {
         public const char VERSION_SEPARATOR = '.';
+        public const char PREFIX = 'v';
 
-        public string Version => Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        public string Version {
 
-        public string FullVersion => typeof(Startup).Assembly.GetName().Version.ToString();
+            get {
+                string versionNumber = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+                return $"{PREFIX}{versionNumber}";
+            }
+
+        }
+
+        public string FullVersion {
+
+            get {
+                string versionNumber = typeof(Startup).Assembly.GetName().Version.ToString();
+                return $"{PREFIX}{versionNumber}";
+            }
+
+        }
 
         public string PublicVersion {
 
             get {
                 int index = Version.IndexOf(VERSION_SEPARATOR, Version.IndexOf(VERSION_SEPARATOR) + 1);
-                return $"v{Version.Substring(0, index)}";
+                return Version.Substring(0, index);
             }
+
         }
     }
 }
